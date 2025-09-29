222cd82c43b96e745ea0f24f6f318e251201735e
Vamsh Amin <vamshamin333@gmail.com>
2025-09-29 22:34:51 +0530

    Async Array Mapping updated

--- map.js
+++ map.js
@@ -1,6 +1,6 @@
 // Write an asynchronous function that takes an array of numbers and returns a new array of Promises where each number is multiplied by 2 after a delay of 500 milliseconds
 
-function multipliedbyTwoWithDelay(arr){
+async function multipliedbyTwoWithDelay(arr){
     return arr.map(num => new Promise((resolve) => {
         setTimeout(() => {
             resolve(num * 2);
@@ -8,6 +8,6 @@ function multipliedbyTwoWithDelay(arr){
     }));
 }
 
-multipliedbyTwoWithDelay([1, 2, 3, 4, 5]).then(results => {
+Promise.all(multipliedbyTwoWithDelay([1, 2, 3, 4, 5])).then(results => {
     console.log(results); // [2, 4, 6, 8, 10] after approximately 500ms
 });
